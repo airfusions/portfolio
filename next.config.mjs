c5764abfedf64fb536b3800243e6de93317ad42d
Shreyansh Parashar <121801340+airfusions@users.noreply.github.com>
2025-01-01 13:38:35 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// postcss.config.mjs
+import autoprefixer from 'autoprefixer';
+import cssnano from 'cssnano';
+import postcssPresetEnv from 'postcss-preset-env';
 
-export default nextConfig;
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    'postcss-preset-env': {
+        stage: 3,
+        features: {
+            'nesting-rules': true,
+        }
+    },
+    cssnano: {
+      preset: 'default'
+    },
+  },
+};
